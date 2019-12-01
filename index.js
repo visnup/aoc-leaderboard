31b14d07d978b32753795acd31a6721c0eb42567
Visnu Pitiyanuvath <visnup@gmail.com>
2019-12-01 16:40:04 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,19 @@
+const fetch = require("node-fetch");
+const observableUserContent = new RegExp(
+  "^https://[\\w-]+.static.observableusercontent.com$"
+);
+
+module.exports = (req, res) => {
+  const {year, id, session} = req.query;
+  console.log({year, id}, req.headers.origin);
+  if (observableUserContent.test(req.headers.origin))
+    res.setHeader("access-control-allow-origin", req.headers.origin);
+  fetch(
+    `https://adventofcode.com/${year}/leaderboard/private/view/${id}.json`,
+    {
+      headers: {
+        cookie: `session=${session}`
+      }
+    }
+  ).then(({body}) => body.pipe(res));
+};
