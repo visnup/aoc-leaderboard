ea078601ef48f20c92bf33c7a0be16eb4bcdbd1b
Visnu Pitiyanuvath <visnup@gmail.com>
2022-11-22 09:07:59 -0800

    Update for Vercel in 2022

--- api/index.js
+++ api/index.js
@@ -1,19 +1,20 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
+
 const observableUserContent = new RegExp(
   "^https://[\\w-]+.static.observableusercontent.com$"
 );
 
-module.exports = (req, res) => {
-  const {year, id, session = process.env.session} = req.query;
-  console.log({year, id}, req.headers.origin);
+export default function (req, res) {
+  const { year, id, session = process.env.SESSION } = req.query;
+  console.log({ year, id }, req.headers.origin);
   if (observableUserContent.test(req.headers.origin))
     res.setHeader("access-control-allow-origin", req.headers.origin);
   fetch(
     `https://adventofcode.com/${year}/leaderboard/private/view/${id}.json`,
     {
       headers: {
-        cookie: `session=${session}`
-      }
+        cookie: `session=${session}`,
+      },
     }
-  ).then(({body}) => body.pipe(res));
-};
+  ).then(({ body }) => body.pipe(res));
+}
